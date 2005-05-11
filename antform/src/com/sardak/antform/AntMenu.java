a786d32b0179a7879952ad5d5dc5d401c10ad3c2
sardak <sardak@a5149dcb-3d11-0410-8d1e-e7c271422401>
2005-05-11 19:04:45 +0000

    no message

--- antform/src/com/sardak/antform/AntMenu.java
+++ antform/src/com/sardak/antform/AntMenu.java
@@ -1,52 +1,171 @@
+ /***************************************************************************\*
+ *                                                                            *
+ *    AntForm form-based interaction for Ant scripts                          *
+ *    Copyright (C) 2005 René Ghosh                                           *
+ *                                                                            *
+ *   This library is free software; you can redistribute it and/or modify it  *
+ *   under the terms of the GNU Lesser General Public License as published by *
+ *   the Free Software Foundation; either version 2.1 of the License, or (at  *
+ *   your option) any later version.                                          *
+ *                                                                            *
+ *   This library is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of               *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser  *
+ *   General Public License for more details.                                 *
+ *                                                                            *
+ *   You should have received a copy of the GNU Lesser General Public License *
+ *   along with this library; if not, write to the Free Software Foundation,  *
+ *   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA              *
+ \****************************************************************************/
 package com.sardak.antform;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Iterator;
 
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Target;
+import org.apache.tools.ant.taskdefs.CallTarget;
 
+import com.sardak.antform.gui.CallBack;
+import com.sardak.antform.types.BaseType;
+import com.sardak.antform.types.Label;
 import com.sardak.antform.types.Link;
+import com.sardak.antform.types.Separator;
 
 /**
+ * AntMenu task definition class.
  * @author René Ghosh
  * 12 janv. 2005
  */
-public class AntMenu extends AbstractTaskWindow implements AntCallBack{
+public class AntMenu extends AbstractTaskWindow implements CallBack{
 	private Target nextTarget = null;
-	
-	
-	
+	private boolean built = false;
+	/**
+	 * Initialisation
+	 */
 	public void init() throws BuildException {
-		super.init();			
+		super.init();
+		properties = new ArrayList();		
 	}
+	/**
+	 * build the visual components
+	 */
+	protected void build(){
+		super.build();		
+		for (Iterator iter = properties.iterator(); iter.hasNext();) {
+			BaseType o = (BaseType) iter.next();
+			if (o.getIf()!=null){
+				if (!getProject().getProperties().containsKey(o.getIf())){
+					continue;
+				}
+			}
+			if (o.getUnless()!=null) {
+				if (getProject().getProperties().containsKey(o.getUnless())){
+					continue;
+				}			
+			}
+			if (o instanceof Label) {
+				Label label = (Label) o;
+				control.getPanel().addLabel(label.getText(), label.getColumns(), label.getRows());
+			} else if (o instanceof Link) {
+				Link link = (Link) o;
+				control.getPanel().addLink(link.getLabel(), link.getTarget());
+			} else if (o instanceof Separator) {
+				control.getPanel().addSeparator();
+			}			
+		}
+		control.getPanel().addBlankSouthPanel();
+		built = true;
+	}
+	
+	/**
+	 * add a configured separator
+	 * @param textProperty
+	 */
+	public void addConfiguredSeparator(Separator separator){		
+		properties.add(separator);
+	}
+	
 	/**
 	 * execute method
 	 */
-	public void execute() throws BuildException {		
-		super.execute();			
-		waitForValidation();
-		if (nextTarget!=null) {
-			nextTarget.execute();
+	public void execute() throws BuildException {
+		preliminaries();
+		if (!built) {
+			build();
+		}
+		super.execute();		
+		control.show();
+		if (dynamic) {
+			built = false;
+			control = null;
+		}
+		if (nextTarget!=null) {					
+			CallTarget callee = (CallTarget) getProject().createTask("antcall");
+			callee.setOwningTarget(getOwningTarget());
+			callee.setTaskName(getTaskName());
+			callee.setLocation(getLocation());
+			callee.setTarget(nextTarget.getName());
+			callee.execute();
 		}
 	}
+	
+	/**
+	 * implement a callback that ports to a 
+	 * target by autumatically setting okMessage and nextTarget
+	 * values 
+	 */
+	public void callbackLink(String target) {		
+		nextTarget=findTargetByName(target);
+		quit = true;		
+	}
+	
 	/**
 	 * add a configured link
 	 */
 	public void addConfiguredLink(Link link) {
 		checkBaseAttributes(link);
-		control.getPanel().addLink(link.getLabel(), link.getTarget());
+		properties.add(link);
 	}
 	
-	public void callback(String message){
+	/**
+	 * add a configured link
+	 */
+	public void addConfiguredLabel(Label label) {
+		checkBaseAttributes(label);
+		properties.add(label);
+	}
+		
+	
+	/**
+	 * callback for control panel
+	 */
+	public void callbackCommand(String message){
+		if (message==null) {
+			quit = true;
+			return;
+		}
 		Hashtable targets = getProject().getTargets();		
 		for (Iterator i=targets.keySet().iterator();i.hasNext();) {
-			String targetName = (String) i.next();
+			String targetName = (String) i.next();			
 			Target target = (Target) targets.get(targetName);
 			if (target.getName().equals(message)) {
 				quit = true;
 				nextTarget = target;
 			}
+		}
+	}
+
+	/**
+	 * Check that the base properties are correctly set
+	 */
+	private void checkBaseAttributes(Label label) {
+		if (label.getText()==null) {
+			super.log("text attribute of the label property "+label.getClass().getName()+" cannot be null.");
+			needFail = true;
 		}
 	}
 	
@@ -61,7 +180,8 @@ public class AntMenu extends AbstractTaskWindow implements AntCallBack{
 		if (link.getTarget()==null) {
 			super.log("property attribute of the property "+link.getClass().getName()+" cannot be null.");
 			needFail = true;
-		}
+		}		
 	}
 
+	
 }
