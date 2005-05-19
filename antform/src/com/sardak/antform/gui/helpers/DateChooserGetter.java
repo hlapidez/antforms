99733390fdeda6a75752d357f1ecdefb3e8af594
sardak <sardak@a5149dcb-3d11-0410-8d1e-e7c271422401>
2005-05-19 20:34:26 +0000

    no message

---
+++ antform/src/com/sardak/antform/gui/helpers/DateChooserGetter.java
@@ -0,0 +1,60 @@
+ /***************************************************************************\*
+ *                                                                            *
+ *    AntForm form-based interaction for Ant scripts                          *
+ *    Copyright (C) 2005 René Ghosh                                           *
+ *                                                                            *
+ *   This library is free software; you can redistribute it and/or modify it  *
+ *   under the terms of the GNU Lesser General Public License as published by *
+ *   the Free Software Foundation; either version 2.1 of the License, or (at  *
+ *   your option) any later version.                                          *
+ *                                                                            *
+ *   This library is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of               *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser  *
+ *   General Public License for more details.                                 *
+ *                                                                            *
+ *   You should have received a copy of the GNU Lesser General Public License *
+ *   along with this library; if not, write to the Free Software Foundation,  *
+ *   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA              *
+ \****************************************************************************/
+package com.sardak.antform.gui.helpers;
+
+import java.awt.Component;
+
+import com.sardak.antform.gui.DateChooser;
+import com.sardak.antform.interfaces.ValueHandle;
+
+/**
+ * @author René Ghosh
+ * 20 mars 2005
+ */
+public class DateChooserGetter implements ValueHandle{
+	private DateChooser chooser;
+	/**
+	 * Constructor
+	 */
+	public DateChooserGetter(DateChooser chooser){
+		this.chooser=chooser;
+	}
+	
+	/**
+	 * get the component value
+	 */
+	public String getValue() {
+		return chooser.getText();
+	}
+	/**
+	 * set selected value
+	 */
+	public void setValue(String s) {
+		chooser.setText(s);
+	}
+	
+	/**
+	 * Return the input component
+	 */
+	public Component getComponent() {
+		return chooser;
+	}
+
+}
