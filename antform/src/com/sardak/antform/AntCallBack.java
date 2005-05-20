72821c8b49774fb2e74422eb9d2ae34fb172fba2
nobody <nobody@a5149dcb-3d11-0410-8d1e-e7c271422401>
2005-05-20 18:29:07 +0000

    This commit was manufactured by cvs2svn to create tag 'arelease'.

--- antform/src/com/sardak/antform/AntCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.sardak.antform;
-
-/**
- * Callback interface for synchronizing execution end with the form approval
- * @author René Ghosh
- */
-public interface AntCallBack {
-	public void callback(String message);	
-}
