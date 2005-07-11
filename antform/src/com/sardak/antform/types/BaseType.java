e1ad1eb596ef8fac5129ba087a647d632371cb0d
patjlm <patjlm@a5149dcb-3d11-0410-8d1e-e7c271422401>
2005-07-11 19:43:55 +0000

    set BaseType as abstract

--- antform/src/com/sardak/antform/types/BaseType.java
+++ antform/src/com/sardak/antform/types/BaseType.java
@@ -1,22 +1,22 @@
- /***************************************************************************\*
-  *                                                                            *
-  *    AntForm form-based interaction for Ant scripts                          *
-  *    Copyright (C) 2005 René Ghosh                                           *
-  *                                                                            *
-  *   This library is free software; you can redistribute it and/or modify it  *
-  *   under the terms of the GNU Lesser General Public License as published by *
-  *   the Free Software Foundation; either version 2.1 of the License, or (at  *
-  *   your option) any later version.                                          *
-  *                                                                            *
-  *   This library is distributed in the hope that it will be useful, but      *
-  *   WITHOUT ANY WARRANTY; without even the implied warranty of               *
-  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser  *
-  *   General Public License for more details.                                 *
-  *                                                                            *
-  *   You should have received a copy of the GNU Lesser General Public License *
-  *   along with this library; if not, write to the Free Software Foundation,  *
-  *   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA              *
-  \****************************************************************************/
+/***************************************************************************\*
+ *                                                                            *
+ *    AntForm form-based interaction for Ant scripts                          *
+ *    Copyright (C) 2005 René Ghosh                                           *
+ *                                                                            *
+ *   This library is free software; you can redistribute it and/or modify it  *
+ *   under the terms of the GNU Lesser General Public License as published by *
+ *   the Free Software Foundation; either version 2.1 of the License, or (at  *
+ *   your option) any later version.                                          *
+ *                                                                            *
+ *   This library is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of               *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser  *
+ *   General Public License for more details.                                 *
+ *                                                                            *
+ *   You should have received a copy of the GNU Lesser General Public License *
+ *   along with this library; if not, write to the Free Software Foundation,  *
+ *   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA              *
+ \****************************************************************************/
 package com.sardak.antform.types;
 
 import org.apache.tools.ant.Project;
@@ -25,38 +25,41 @@ import com.sardak.antform.gui.ControlPanel;
 import com.sardak.antform.interfaces.ValueHandle;
 
 /**
- * @author René Ghosh
- * 1 avr. 2005
+ * @author René Ghosh 1 avr. 2005
  */
-public class BaseType {
+public abstract class BaseType {
 	private String If, unless;
+
 	public String getIf() {
 		return If;
 	}
+
 	public void setIf(String if1) {
 		If = if1;
 	}
+
 	public String getUnless() {
 		return unless;
 	}
+
 	public void setUnless(String unless) {
 		this.unless = unless;
 	}
+
 	public boolean shouldBeDisplayed(Project project) {
-	    boolean shouldBeDisplayed = true;
-	    if (getIf()!=null){
-			if (!project.getProperties().containsKey(getIf())){
-			    shouldBeDisplayed = false;
+		boolean shouldBeDisplayed = true;
+		if (getIf() != null) {
+			if (!project.getProperties().containsKey(getIf())) {
+				shouldBeDisplayed = false;
 			}
 		}
-		if (getUnless()!=null) {
-			if (project.getProperties().containsKey(getUnless())){
-			    shouldBeDisplayed = false;
-			}			
+		if (getUnless() != null) {
+			if (project.getProperties().containsKey(getUnless())) {
+				shouldBeDisplayed = false;
+			}
 		}
 		return shouldBeDisplayed;
 	}
-	public ValueHandle addToControlPanel(ControlPanel panel) {
-	    return null;
-	}
+
+	public abstract ValueHandle addToControlPanel(ControlPanel panel);
 }
