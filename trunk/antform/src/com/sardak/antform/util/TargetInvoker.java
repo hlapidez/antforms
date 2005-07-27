f3b10e4f512f26947b99683f3000f388ff1fdec5
patjlm <patjlm@a5149dcb-3d11-0410-8d1e-e7c271422401>
2005-07-27 17:34:57 +0000

    bug fix: make sure background threads finish before build ends.

--- trunk/antform/src/com/sardak/antform/util/TargetInvoker.java
+++ trunk/antform/src/com/sardak/antform/util/TargetInvoker.java
@@ -1,6 +1,9 @@
 package com.sardak.antform.util;
 
+import org.apache.tools.ant.BuildEvent;
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.BuildListener;
+import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.taskdefs.CallTarget;
 
@@ -10,9 +13,10 @@ import org.apache.tools.ant.taskdefs.CallTarget;
  * This class allows to call an ant target, in the same thread or in a new one.
  * It is an ant task itself. 
  */
-public class TargetInvoker extends CallTarget implements Runnable {
+public class TargetInvoker extends CallTarget implements Runnable, BuildListener {
 
 	private boolean background = false;
+	private Thread createdThread;
 	
 	public TargetInvoker(Task parentTask, String target) {
 		configure(parentTask, target);
@@ -38,12 +42,45 @@ public class TargetInvoker extends CallTarget implements Runnable {
 		if (background) {
 			Thread t = new Thread(this);
 	        t.start();
+	        createdThread = t;
+	        getProject().addBuildListener(this);
 		} else {
 			run();
 		}
 	}
 	
-	public void configure(Task parentTask, String target) {
+	public void buildStarted(BuildEvent evt) {
+	}
+
+	public void buildFinished(BuildEvent evt) {
+		if (createdThread != null && getProject().equals(evt.getProject())) {
+			if (createdThread.isAlive()) {
+				log("Waiting for background threads completion...", Project.MSG_VERBOSE);
+				try {
+					createdThread.join();
+				} catch (InterruptedException ie) {
+					log("Thread " + createdThread.getName() + " got interrupted: " + ie.getMessage(), Project.MSG_DEBUG);
+				}
+			}
+		}
+	}
+
+	public void targetStarted(BuildEvent evt) {
+	}
+
+	public void targetFinished(BuildEvent evt) {
+	}
+
+	public void taskStarted(BuildEvent evt) {
+	}
+
+	public void taskFinished(BuildEvent evt) {
+	}
+
+	public void messageLogged(BuildEvent evt) {
+	}
+
+	private void configure(Task parentTask, String target) {
 		setProject(parentTask.getProject());
 		setOwningTarget(parentTask.getOwningTarget());
 		setTaskName(parentTask.getTaskName());
@@ -51,7 +88,7 @@ public class TargetInvoker extends CallTarget implements Runnable {
 		setTarget(target);
 	}
 
-	public void configure(Task parentTask, String target, boolean parallel) {
+	private void configure(Task parentTask, String target, boolean parallel) {
 		configure(parentTask, target);
 		this.background = parallel; 
 	}
