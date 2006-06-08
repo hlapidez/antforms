2247ec98777f88c73a15d292ff8fb64ef316c704
patjlm <patjlm@a5149dcb-3d11-0410-8d1e-e7c271422401>
2006-06-08 15:50:54 +0000

    Fix constructor

--- trunk/antform/src/com/sardak/antform/types/Button.java
+++ trunk/antform/src/com/sardak/antform/types/Button.java
@@ -56,6 +56,7 @@ public class Button extends BaseType implements ActionComponent, Focusable {
 		super();
 		setLabel(text);
 		setTarget(target);
+		type = actionType;
 	}
 	
 	public String getLabel() {
