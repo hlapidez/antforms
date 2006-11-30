b9120696287b3f687f5df3faac518aa521f11745
patjlm <patjlm@a5149dcb-3d11-0410-8d1e-e7c271422401>
2006-11-30 09:57:52 +0000

    v2.0 beta 2

---
+++ tags/antform-2.0beta2/src/com/sardak/antform/test/TableTest.java
@@ -0,0 +1,36 @@
+package com.sardak.antform.test;
+
+import javax.swing.UIManager;
+
+import com.sardak.antform.gui.Control;
+import com.sardak.antform.gui.ControlPanel;
+import com.sardak.antform.types.Table;
+
+/**
+ * @author René Ghosh
+ * 1 avr. 2005
+ */
+public class TableTest {
+public static void main(String[] args) {
+	try {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	
+	Control control = new Control(new CallbackTest(), "Table test", null, null, false);
+	ControlPanel panel = control.getPanel();
+	Table t = new Table();
+	t.setLabel("a table");
+	t.setProperty("prop");
+	t.setEditable(true);
+	t.setColumns("col1,col2,col3");
+	t.setData("d1,d2,d3;d1,d2,d3;d1,d2,d3");
+	t.setRowSeparator(";");
+	t.setColumnSeparator(",");
+	t.setEscapeSequence("\\");
+	t.addToControlPanel(panel);
+	control.show();
+	System.exit(0);
+}
+}
